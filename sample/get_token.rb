f13491f1c7fb0c13d13fbee5fddfed122d7eec1a
Yoshihiro Kameda <kameda.sbng@gmail.com>
2013-12-21 21:20:15 +0900

    add samples

--- sample/get_token.rb
+++ sample/get_token.rb
@@ -1,7 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 def main
-  token = Feedly.get_token
+  feedly = Feedly.new
+  puts '1. Access this url. Then redirect http://localhost?code=???&status= .'
+  puts feedly.auth_url
+
+  puts '2. Please input code'
+  code = gets
+
+  token = feedly.get_token_by_code(code)
+  puts "access_token: " + token.access_token
+  puts "refresh_token: " + token.refresh_token
 end
 
 case $0
