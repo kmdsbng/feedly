00a5ac6cfc92b4f005d1d9c14220bb9631204801
Yoshihiro Kameda <kameda.sbng@gmail.com>
2013-12-23 03:15:29 +0900

    add delete_subscriptions method.

--- lib/feedly.rb
+++ lib/feedly.rb
@@ -80,6 +80,22 @@ class Feedly
     true
   end
 
+
+  def delete_subscriptions(feed_id)
+    argv = {}
+    url = make_url('subscriptions/' + URI.encode_www_form_component(feed_id), argv)
+    uri = URI(url)
+    req = Net::HTTP::Delete.new(uri.request_uri)
+    req['Authorization'] = "OAuth #{self.access_token}"
+
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    handle_errors(response)
+    true
+  end
+
   def make_url(path, argv)
     base_url = Feedly::API_URL + path
     query = argv.map {|k, v|
