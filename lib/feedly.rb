ac8106bf811f73496e59403dd5fb5def04bfcf9a
Yoshihiro Kameda <kameda.sbng@gmail.com>
2013-12-23 03:06:15 +0900

    Add post_subscriptions method. Yeah!

--- lib/feedly.rb
+++ lib/feedly.rb
@@ -58,6 +58,28 @@ class Feedly
     api_get('search/feeds', :q => q, :n => n)
   end
 
+  def get_feeds(feed_id)
+    api_get('feeds/' + URI.encode_www_form_component(feed_id))
+  end
+
+  def post_subscriptions(feed_id)
+    argv = {}
+    url = make_url('subscriptions', argv)
+    uri = URI(url)
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req['Authorization'] = "OAuth #{self.access_token}"
+    req['Content-type'] = 'application/json'
+
+    req.body = {:id => feed_id}.to_json
+
+    response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    handle_errors(response)
+    true
+  end
+
   def make_url(path, argv)
     base_url = Feedly::API_URL + path
     query = argv.map {|k, v|
