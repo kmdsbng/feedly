5c7ef8ccd521510df4e980846fd656dda9584cb6
Yoshihiro Kameda <kameda.sbng@gmail.com>
2013-12-28 10:51:37 +0900

    change sample.

--- sample/get_token.rb
+++ sample/get_token.rb
@@ -3,13 +3,15 @@ require 'feedly'
 
 def main
   feedly = Feedly.new
-  puts '1. Access this url. Then redirect http://localhost?code=???&status= .'
+  puts '1. Access this url. Then redirect https://cloud.feedly.com/feedly.html?code=???&status= .'
   puts feedly.auth_url
 
   puts '2. Please input code'
+  print '> '
   code = gets.chomp
 
   token = feedly.get_token_by_code(code)
+  puts "OK. success to get tokens"
   puts "access_token: " + token["access_token"]
   puts "refresh_token: " + token["refresh_token"]
 end
